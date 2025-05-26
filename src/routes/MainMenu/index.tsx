e196db6d8fe087a66c7e1d13da7cdd458276edfc
rafael-abacherly <rafael.abacherly@gmail.com>
2025-05-25 21:17:51 -0300

    MFLP-38: Tela de menu principal

--- src/routes/MainMenu/index.tsx
+++ src/routes/MainMenu/index.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import Button from '../../components/Button';
+import { useNavigate } from 'react-router-dom';
 
 const MainMenu: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <p>Hello! I am the MainMenu page</p>
+    <div className='d-center container' 
+      style={{ display: 'flex', flexDirection: 'column', gap: '20px', justifyContent: 'center',
+      alignItems: 'center', height: '100vh', paddingBottom: '100px' }}>
+
+      <img src="src/assets/img/logo-pokariba.png" alt="Logo" style={{ width: '300px' }} />
+
+      <div style={{ display: 'flex', gap: '20px', justifyContent: 'center', alignItems: 'center' }}>
+        <Button color="white" size="lg" border={false} style={{ width: '180px' }} onClick={() => navigate('\Game')}>
+          Jogar
+        </Button>
+        <Button color="white" size="lg" border={false} style={{ width: '180px' }} onClick={() => navigate('\Store')}>
+          Loja
+          </Button>
+        <Button color="white" size="lg" border={false} style={{ width: '180px' }} onClick={() => navigate('\Help')}>
+          Regras
+          </Button>
+      </div>
     </div>
   );
 };
