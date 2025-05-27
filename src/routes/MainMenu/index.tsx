8432ecf7f388b2a99a8af3b4699ef7f7cd4951e3
rafael-abacherly <rafael.abacherly@gmail.com>
2025-05-27 00:34:04 -0300

    MFLP-38: MainMenu

--- src/routes/MainMenu/index.tsx
+++ src/routes/MainMenu/index.tsx
@@ -1,27 +1,50 @@
 import React from 'react';
 import Button from '../../components/Button';
 import { useNavigate } from 'react-router-dom';
+import SideBar from '../SideBar';
+import logo from '../../assets/img/logo-pokariba.png';
 
 const MainMenu: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <div className='d-center container' 
-      style={{ display: 'flex', flexDirection: 'column', gap: '20px', justifyContent: 'center',
-      alignItems: 'center', height: '100vh', paddingBottom: '100px' }}>
+    <div style={{ display: 'flex', height: '100vh' }}>
+      <div style={{ position: 'absolute', left: 0, top: 0, width: '250px', height: '100vh' }}>
+        <SideBar />
+      </div>
 
-      <img src="src/assets/img/logo-pokariba.png" alt="Logo" style={{ width: '300px' }} />
+      <div className='d-center container'  
+        style={{ 
+          display: 'flex', 
+          flexDirection: 'column', 
+          alignItems: 'center', 
+          justifyContent: 'center',
+          flexGrow: 1, 
+          height: '100vh', 
+          gap: '30px',
+          marginLeft: '250px'
+        }}>
 
-      <div style={{ display: 'flex', gap: '20px', justifyContent: 'center', alignItems: 'center' }}>
-        <Button color="white" size="lg" border={false} style={{ width: '180px' }} onClick={() => navigate('\Game')}>
-          Jogar
-        </Button>
-        <Button color="white" size="lg" border={false} style={{ width: '180px' }} onClick={() => navigate('\Store')}>
-          Loja
+        <img src={logo} alt="Logo" style={{ width: '300px', display: 'block' }} />
+
+        <div style={{ 
+          display: 'flex', 
+          flexDirection: 'row', 
+          gap: '20px', 
+          alignItems: 'center', 
+          justifyContent: 'center', 
+          width: '100%' 
+        }}>
+          <Button color="white" style={{ width: '180px' }} onClick={() => navigate('/game')}>
+            Jogar
           </Button>
-        <Button color="white" size="lg" border={false} style={{ width: '180px' }} onClick={() => navigate('\Help')}>
-          Regras
+          <Button color="white" style={{ width: '180px' }} onClick={() => navigate('/store')}>
+            Loja
           </Button>
+          <Button color="white" style={{ width: '180px' }} onClick={() => navigate('/help')}>
+            Regras
+          </Button>
+        </div>
       </div>
     </div>
   );
